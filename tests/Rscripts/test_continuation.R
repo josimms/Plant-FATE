3caa39da57f7472e123c189b0f9e81269da82fb5
Jaideep <jaideep777@gmail.com>
2024-04-02 15:25:46 +0200

    [hotfix/save_restore] tested save-restore of evolutionary sim

--- tests/Rscripts/test_continuation.R
+++ tests/Rscripts/test_continuation.R
@@ -9,28 +9,33 @@ main_dir   = paste0(dir,"/cont_test_main")
 setwd(paste0("~/codes/Plant-FATE/",spinup_dir))
 dat2_spin = read.csv("Y_PFATE.csv")
 dat_spin = read.csv("D_PFATE.csv")
+traits_spin = read.csv("traits.csv")
 Yend = max(dat2_spin$YEAR)
 
 setwd(paste0("~/codes/Plant-FATE/",main_dir))
 dat2_main = read.csv("Y_PFATE.csv")
 dat_main = read.csv("D_PFATE.csv")
+traits_main = read.csv("traits.csv")
 
 setwd(paste0("~/codes/Plant-FATE/",ref_dir))
 dat2_ref = read.csv("Y_PFATE.csv")
 dat_ref = read.csv("D_PFATE.csv")
+traits_ref = read.csv("traits.csv")
 
 
 dat2_main = dat2_main %>% filter(YEAR > Yend)
 dat_main = dat_main %>% filter(YEAR > Yend)
+traits_main = traits_main %>% filter(YEAR > Yend)
 
 dat2 = rbind(dat2_spin, dat2_main)
 dat = rbind(dat_spin, dat_main)
+traits = rbind(traits_spin, traits_main)
 
-plot_data = function(dat, dat2, main){
+plot_data = function(dat, dat2, traits, main){
   n_species = length(unique(dat2$PID))
   n_year = length(unique(dat2$YEAR))
   
-  par(mfrow=c(1,3), mar=c(6,6,1,1), oma=c(1,1,2,1), cex.lab=1.3, cex.axis=1.2, mgp=c(3.2,1,0), las=1)
+  par(mfrow=c(1,4), mar=c(6,6,1,1), oma=c(1,1,2,1), cex.lab=1.3, cex.axis=1.2, mgp=c(3.2,1,0), las=1)
   
   seeds = dat2 %>% select(YEAR, PID, SEEDS) %>% spread(value = "SEEDS", key = "PID")
   matplot(seeds$YEAR, seeds[,-1], lty=1, col=rainbow(n = n_species+1, start = 0, end = 0.85, alpha = 0.5), type="l",
@@ -44,7 +49,12 @@ plot_data = function(dat, dat2, main){
   
   plot(dat$GPP~dat$YEAR, type="l", col="green4", xlab="Time (years)", ylab="GPP")
   abline(v=1200, col="pink")
+
+  Tr = traits %>% select(YEAR, SPP, WD) %>% spread(value = "WD", key = "SPP")
+  matplot(Tr$YEAR, Tr[,-1], lty=1, col=rainbow(n = n_species+1, start = 0, end = 0.85, alpha = 0.5), type="l",
+          las=1, xlab="Time (years)", ylab="Species wood\ndensity", log="", main=main)
+  abline(v=1200, col="pink")
 }
 
-plot_data(dat_ref, dat2_ref, "Ref")
-plot_data(dat, dat2, "Main")
+plot_data(dat_ref, dat2_ref, traits_ref, "Ref")
+plot_data(dat, dat2, traits, "Main")
