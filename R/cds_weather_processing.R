1c731aff4d0b79e714a573a4e148659d36fdd8de
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-07-02 12:56:04 +0200

    cds weather processing optimised with ai

--- R/cds_weather_processing.R
+++ R/cds_weather_processing.R
@@ -1,134 +1,110 @@
 # READING NC FILE
 
 ###
-# Function
+# NEW function
 ###
 
 reading_nc <- function() {
-  # Path
-  path_nc <- "./inst/python/"
-  path_test <- "./tests/data/"
+  library(ncdf4)
+  library(bigleaf)
+  library(data.table)
   
-  # Variables
-  variables <- c('t2m', # 2m_temperature 
-                 'lai_hv', # leaf_area_index_high_vegetation
-                 'ssrd', # surface_solar_radiation_downwards
-                 'tvh', # type_of_high_vegetation
-                 'swvl1', # volumetric_soil_water_layer_1
-                 'swvl2', # volumetric_soil_water_layer_2
-                 'r') # relative humidity
+  path_nc <- "/home/josimms/Documents/Austria/Plant-FATE/inst/python"
+  path_test <- "/home/josimms/Documents/Austria/Plant-FATE/tests/data"
   
-  # Reading nc files
-  for (variable in variables) {
-    # LIST FILES
-    nc_files <- list.files(path = path_nc,
-                           pattern = "download")
+  variables <- c('t2m', 'ssrd', 'swvl1', 'swvl2')
+  
+  # Pre-allocate lists
+  nc_data <- vector("list", length(variables))
+  names(nc_data) <- variables
+  
+  nc_files <- list.files(path = path_nc, pattern = "download", full.names = TRUE)
+  
+  # Coordinates
+  lon_target <- 17.483333
+  lat_target <- 60.083333
+  
+  dataset_cds_raw <- list()
+  for (i in 1:length(nc_files)) {
+    nc <- nc_open(nc_files[i])
     
-    # IMPORT THE FILE
-    nc_temp <- list()
-    dates_temp <- list()
-    for (i in 1:length(nc_files)) {
-      temp <- ncdf4::nc_open(paste0(path_nc, nc_files)[i])
-      
-      lon <- ncdf4::ncvar_get(temp, "longitude")
-      lon_index <- which.min(abs(lon-17.483333)) # TODO: replace the coordinate
-      lat <- ncdf4::ncvar_get(temp, "latitude", verbose = F)
-      lat_index <- which.min(abs(lat-60.083333)) # TODO: replace the coordinate
-      
-      dates_temp[[i]] <- ncdf4::ncvar_get(temp, temp$dim$time)
-      
-      nc_temp[[i]] <- ncdf4::ncvar_get(temp, 
-                                       varid = variable, 
-                                       start = c(lon_index, lat_index, 1),
-                                       count = c(1, 1, -1))
+    lon <- ncvar_get(nc, "longitude")
+    lat <- ncvar_get(nc, "latitude")
+    lon_index <- which.min(abs(lon - lon_target))
+    lat_index <- which.min(abs(lat - lat_target))
+    
+    dates <- ncvar_get(nc, nc$dim$time)
+    
+    for (var in variables) {
+      nc_data[[var]] <- c(nc_data[[var]], 
+                          ncvar_get(nc, varid = var, 
+                                    start = c(lon_index, lat_index, 1),
+                                    count = c(1, 1, -1)))
     }
-    nc_variable <- unlist(nc_temp)
-    dates <- unlist(dates_temp)
     
+    nc_close(nc)
     
-    # VARIABLES FORMATTED
-    # TODO: units
-    if (variable == "t2m") {
-      tempature = nc_variable - 273.15 # K to 'C
-    } else if (variable == "lai_hv") {
-      leaf_area = nc_variable
-    } else if (variable == "ssrd") {
-      PPDF = bigleaf::Rg.to.PPFD(nc_variable) # J m**-2 s-1 = W m**-2 s-1 to umol m-2 s-1
-    } else if (variable == "tvh") {
-      vegetation = nc_variable
-    } else if (variable == "swvl1") {
-      soil_water1 = nc_variable
-    } else if (variable == "swvl2") {
-      soil_water2 = nc_variable
-    } else if (variable == "r") {
-      rh = nc_variable
-    }
-    print(paste(variable, "done! :D"))
+    # Create data.table
+    dataset_cds_raw_year <- data.table(
+      t2m = nc_data$t2m  - 273.15,
+      ssrd = Rg.to.PPFD(nc_data$ssrd),
+      swvl1 = nc_data$swvl1,
+      swvl2 = nc_data$swvl2,
+      date = as.POSIXct(dates*3600, origin = "1900-01-01", tz = "GMT")
+    )
+    
+    dataset_cds_raw[[i]] <- dataset_cds_raw_year
   }
   
-  # MAKING DATAFRAME
-  dataset_cds_raw <- data.frame(tempature, leaf_area, PPDF, vegetation, soil_water1, soil_water2, rh)
-  names(dataset_cds_raw) <- c(variables)
-  dataset_cds_raw$date <- as.POSIXct(dates*3600, origin = "1900-01-01 00:00:00.0", tz = "GMT")
-  dataset_cds_raw$vpd <- 10*bigleaf::rH.to.VPD(dataset_cds_raw$r, dataset_cds_raw$t2m) # TODO: check that rh is right!
+  dataset_cds_raw_all <- rbindlist(dataset_cds_raw)
   
-  # MONTH
-  dataset_cds_raw$YM <- substring(dataset_cds_raw$date, 1, 7)
-  monthy_dataset <- aggregate(. ~ YM, data = dataset_cds_raw, mean)
-  monthy_dataset$ssrd_max <- aggregate(. ~ YM, data = dataset_cds_raw[,c("ssrd", "YM")], max)$ssrd
-  save(monthy_dataset, file = paste0(path_test, "montly_dataset.csv"))
+  #dataset_cds_raw[, vpd := 10 * rH.to.VPD(r, t2m)]
+  dataset_cds_raw_all[, YM := format(date, "%Y-%m")]
+  dataset_cds_raw_all[, YMD := format(date, "%Y-%m-%d")]
   
-  # DAY
-  dataset_cds_raw$YMD <- substring(dataset_cds_raw$date, 1, 10)
-  daily_dataset <- aggregate(. ~ YMD, data = dataset_cds_raw[,-c(8)], mean)
-  daily_dataset$ssrd_max <- aggregate(. ~ YMD, data = dataset_cds_raw[,c("ssrd", "YMD")], max)$ssrd
-  save(daily_dataset, file = paste0(path_test, "daily_dataset.csv"))
+  # Monthly aggregation
+  monthy_dataset <- dataset_cds_raw_all[, lapply(.SD, mean), by = YM, .SDcols = -c("YMD", "date")]
+  monthy_dataset[, ssrd_max := dataset_cds_raw_all[, .(ssrd_max = max(ssrd)), by = YM]$ssrd_max]
   
-  ###
-  # PLOT
-  ###
+  # Daily aggregation
+  daily_dataset <- dataset_cds_raw_all[, lapply(.SD, mean), by = YMD, .SDcols = -c("YM", "date")]
+  daily_dataset[, ssrd_max := dataset_cds_raw_all[, .(ssrd_max = max(ssrd)), by = YMD]$ssrd_max]
   
-  # MONTHLY
-  par(mfrow = c(2, 3))
-  plot(monthy_dataset$date, monthy_dataset$t2m, main = "Temperature", xlab = "Dates", ylab = "Degrees C")
-  title(sub = paste("Percentage missing", round(100*sum(is.na(monthy_dataset$t2m))/nrow(monthy_dataset), 2), "%"))
-  plot(monthy_dataset$date, monthy_dataset$vpd, main = "VPD", xlab = "Dates", ylab = "hPa")
-  title(sub = paste("Percentage missing", round(100*sum(is.na(monthy_dataset$vpd))/nrow(monthy_dataset), 2), "%"))
-  plot(monthy_dataset$date, monthy_dataset$ssrd, main = "PAR", xlab = "Dates", ylab = "umol m-2 s-1") # TODO: units!
-  title(sub = paste("Percentage missing", round(100*sum(is.na(monthy_dataset$ssrd))/nrow(monthy_dataset), 2), "%"))
-  plot(monthy_dataset$date, monthy_dataset$ssrd_max, main = "PAR_max", xlab = "Dates", ylab = "umol m-2 s-1") # TODO: units!
-  title(sub = paste("Percentage missing", round(100*sum(is.na(monthy_dataset$ssrd_max))/nrow(monthy_dataset), 2), "%"))
-  plot(monthy_dataset$date, monthy_dataset$swvl2, main = "SWP", xlab = "Dates", ylab = "-kPa (should be MPa for PlantFATE") # TODO: units!
-  title(sub = paste("Percentage missing", round(100*sum(is.na(monthy_dataset$swvl2))/nrow(monthy_dataset), 2), "%"))
-  plot(monthy_dataset$ssrd, monthy_dataset$ssrd_max, main = "Gobal: Mean vs Max", xlab = "Mean", ylab = "Max")
-  abline(lm(ssrd_max ~ ssrd, data = monthy_dataset, na.action = na.omit ), col = "red")
-  title(sub = paste("Gradient", round(coefficients(lm(ssrd_max ~ ssrd, data = monthy_dataset, na.action = na.omit ))[2], 2)))
+  # Save datasets
+  fwrite(monthy_dataset, file = file.path(path_test, "montly_dataset.csv"))
+  fwrite(daily_dataset, file = file.path(path_test, "daily_dataset.csv"))
   
-  # DAILY
-  par(mfrow = c(2, 3))
-  plot(daily_dataset$date, daily_dataset$t2m, main = "Temperature", xlab = "Dates", ylab = "Degrees C")
-  title(sub = paste("Percentage missing", round(100*sum(is.na(daily_dataset$t2m))/nrow(daily_dataset), 2), "%"))
-  plot(daily_dataset$date, daily_dataset$vpd, main = "VPD", xlab = "Dates", ylab = "hPa")
-  title(sub = paste("Percentage missing", round(100*sum(is.na(daily_dataset$vpd))/nrow(daily_dataset), 2), "%"))
-  plot(daily_dataset$date, daily_dataset$ssrd, main = "PAR", xlab = "Dates", ylab = "umol m-2 s-1") # TODO: units!
-  title(sub = paste("Percentage missing", round(100*sum(is.na(daily_dataset$ssrd))/nrow(daily_dataset), 2), "%"))
-  plot(daily_dataset$date, daily_dataset$ssrd_max, main = "PAR_max", xlab = "Dates", ylab = "umol m-2 s-1") # TODO: units!
-  title(sub = paste("Percentage missing", round(100*sum(is.na(daily_dataset$ssrd_max))/nrow(daily_dataset), 2), "%"))
-  plot(daily_dataset$date, daily_dataset$swvl2, main = "SWP", xlab = "Dates", ylab = "-kPa (should be MPa for PlantFATE") # TODO: units!
-  title(sub = paste("Percentage missing", round(100*sum(is.na(daily_dataset$swvl2))/nrow(daily_dataset), 2), "%"))
-  plot(daily_dataset$ssrd, daily_dataset$ssrd_max, main = "Gobal: Mean vs Max", xlab = "Mean", ylab = "Max")
-  abline(lm(ssrd_max ~ ssrd, data = daily_dataset, na.action = na.omit ), col = "red")
-  title(sub = paste("Gradient", round(coefficients(lm(ssrd_max ~ ssrd, data = daily_dataset, na.action = na.omit ))[2], 2)))
+  # Plotting function (reuse for both monthly and daily)
+  plot_data <- function(data, title_prefix, monthly) {
+    par(mfrow = c(3, 2))
+    if (monthly) {
+      data$YM <- paste0(data$YM, "-01")
+      data$date <- as.Date(data$YM, format = "%Y-%m-%d")
+    } else {
+      data$date <- as.Date(data$YMD, format = "%Y-%m-%d")
+    }
+    
+    
+    # "vpd",
+    plot_vars <- c("t2m", "ssrd", "ssrd_max", "swvl2")
+    for (var in plot_vars) {
+      plot(data$date, data[[var]], main = paste(title_prefix, var), 
+           xlab = "Dates", ylab = switch(var,
+                                         t2m = "Degrees C",
+                                         ssrd = "umol m-2 s-1",
+                                         ssrd_max = "umol m-2 s-1",
+                                         swvl2 = "-kPa"))
+      title(sub = sprintf("Percentage missing: %.2f%%", 100 * sum(is.na(data[[var]])) / nrow(data)))
+    }
+    plot(data$ssrd, data$ssrd_max, main = "Global: Mean vs Max", xlab = "Mean", ylab = "Max")
+    abline(lm(ssrd_max ~ ssrd, data = data), col = "red")
+    title(sub = sprintf("Gradient: %.2f", coef(lm(ssrd_max ~ ssrd, data = data))[2]))
+  }
   
-  # AGAINST HYYTIALA DATA
-
-  # TODO: check against hyytiälä data
-  
-  ###
-  # DONE AND RETURN
-  ###
+  # Plot monthly and daily data
+  plot_data(monthy_dataset, "Monthly", monthly = TRUE)
+  plot_data(daily_dataset, "Daily", monthly = FALSE)
   
   beepr::beep(3)
-  return(dataset_cmip)
+  return(dataset_cds_raw)
 }
-
