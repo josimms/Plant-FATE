b6c1260fe53172d840095c8a5d51905eadd57b3c
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-07-02 12:57:34 +0200

    python code updated / added to speed up the data processing

--- inst/python/api_era5.py
+++ inst/python/api_era5.py
@@ -12,7 +12,7 @@ import cdsapi
 
 c = cdsapi.Client()
  
-for year in range(1960, 2023): # Hyytiälä was planted in 1962
+for year in range(2011, 2023): # Hyytiälä was planted in 1962
 	c.retrieve(
 	    'reanalysis-era5-single-levels',
 	    {
@@ -27,4 +27,21 @@ for year in range(1960, 2023): # Hyytiälä was planted in 1962
 	    },
 	    str(year)+'download.nc')
 
+
+# After big datasets are downloaded need to download the dew point to get RH
+
+#for year in range(1960, 2023): # Hyytiälä was planted in 1962
+#	c.retrieve(
+#	    'reanalysis-era5-single-levels',
+#	    {
+#		'product_type': 'reanalysis',
+#		'format': 'netcdf',
+#		'variable': '2m_dew_temperature', # TODO: find the correct variable name!
+#		'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12',],
+#		'year': year,
+#		'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31',],
+#		'time': ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',],
+#		'area': [61.9, 24.2, 61.8, 25.3,],
+#	    },
+#	    str(year)+'download.nc')
    
