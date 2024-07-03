566a394967e05ef9c1c36d649bfa674ee89cc11e
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-07-03 10:43:47 +0200

    CO2 weather file read and added to the enddataset

--- R/cmip6_weather_processing.R
+++ R/cmip6_weather_processing.R
@@ -1,4 +1,6 @@
 sort_CMIP <- function() {
+  library(ncdf4)
+  
   # Constants
   TEMP_DIR <- "~/Documents/Austria/Plant-FATE/inst/cmip6_data/temp"
   ANSWER_DIR <- "~/Documents/Austria/Plant-FATE/inst/cmip6_data/"
@@ -8,10 +10,8 @@ sort_CMIP <- function() {
   
   # Process each variable
   ssp_scenario = 245
-  parameters <- c("tas", "hurs", "rsds")
+  parameters <- c("tas", "hurs", "rsds") # CO2 Processed Seperately
   results <- list()
-  
-  # CO2 Processed Seperately
   
   for (variable in parameters) {
     spp_files <- list.files(path = TEMP_DIR,
@@ -32,16 +32,23 @@ sort_CMIP <- function() {
               as.Date("2100-12-30"), 
               length.out = length(results$tas)) # TODO: dates are wrong here, but want graphs
   
-  # Get soil potenital from historical data
+  # Get soil potential from historical data
   monthy_dataset <- fread(file.path(path_test, "ERAS_Monthly.csv"))
   daily_dataset <- fread(file.path(path_test, "ERAS_dataset.csv"))
+  
+  # CO2 importing
+  spp_files <- list.files(path = TEMP_DIR,
+                          pattern = "co2",
+                          full.names = TRUE)
+  co2 <- process_files_co2(spp_files, "co2mass", TARGET_LON, TARGET_LAT)
 
   # Create final dataset
   dataset_cmip <- data.table::data.table(date = date,
                                          t2m = results$tas,
                                          ssrd = results$rsds,
                                          vpd = results$VPD,
-                                         sp = rep(daily_dataset$sp, length.out = length(results$tas)))
+                                         sp = rep(daily_dataset$sp, length.out = length(results$tas)),
+                                         co2 = co2)
   dataset_cmip[, YMD := format(date, "%Y-%m-%d")]
   dataset_cmip[, YM := format(date, "%Y-%m")]
   # NOTE: Mean and max the same as the values are from a daily source!
@@ -65,7 +72,10 @@ sort_CMIP <- function() {
   return("Data processed look to files for the datasets")
 }
 
+###
 # Helper functions
+###
+
 process_files <- function(files, variable, target_lon, target_lat) {
   spp_temp <- lapply(files, function(file) {
     temp <- nc_open(file)
@@ -83,6 +93,19 @@ process_files <- function(files, variable, target_lon, target_lat) {
   unlist(spp_temp)
 }
 
+process_files_co2 <- function(files, variable, target_lon, target_lat) {
+  spp_temp <- lapply(files, function(file) {
+    temp <- nc_open(file)
+    on.exit(nc_close(temp))
+    
+    ncvar_get(temp, varid = "co2mass", 
+              start = c(1), 
+              count = c(-1))
+  })
+  unlist(spp_temp)
+}
+
+
 transform_variable <- function(variable, spp) {
   switch(variable,
          "pr" = {
@@ -99,9 +122,6 @@ transform_variable <- function(variable, spp) {
          "hurs" = {
            if (any(spp < 0)) warning("RH contains negative values")
            spp  # %
-         },
-         "co2" = {
-           spp
          },
          stop("Unsupported variable: ", variable)
   )
