e8e19bde270da2b90ab1608531f670914d14e8d9
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-08-21 12:49:38 +0200

    phydro aggregation file

---
+++ R/nphydro_in_PlantFATE.R
@@ -0,0 +1,23 @@
+###
+# Life History
+###
+
+# TODO: calibration files with different infrastructure parameter
+
+# TODO: plot growth of one plant with different levels of belowground infrastructure
+  # TODO: how should this relate to the root parameters?
+
+###
+# 
+###
+
+amazon_sim <- run_simulation("tests/params/p_test_v2.ini", 1000, 1050, "test_3spp_100yr")
+
+# TODO: calibration files updated with the infrastructure parameter
+
+# TODO: plot the amazon with different levels of underground investment
+  # TODO: how should this relate to the root parameters?
+
+
+
+
