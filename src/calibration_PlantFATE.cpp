7b3354f34fceb5b84b7f7018dc05b420eca2e3ec
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-08-26 13:16:32 +0200

    Calibration files added - for loop for one parameter in life history and plantfate seperated

---
+++ src/calibration_PlantFATE.cpp
@@ -0,0 +1,30 @@
+#include "plantfate_patch.h"
+#include <filesystem>
+#include <csvrow.h>
+using namespace std;
+
+int parameter_test_pf(double lower_bound, double upper_bound, std::string params_file) {
+  // Reading in the parameters
+  Patch::Patch(std::string params_file)
+  // what is the output of this? How do I access the structures that are created in the function?
+  
+  // Parameter range
+  for (int i = lower_bound: i++; i < upper_bound) {
+    // Change the parameter value
+    I.infra_translation = i;
+    
+    // Run the model for each parameter
+    try {
+      // TODO: How do you run the lh model?
+      double result = runModel(parm);
+      
+    } catch (const std::exception& e) {
+      // TODO: make an output file for this?
+      cout << "Error with parameter " << parm << ": " << e.what() << endl;
+      
+    }
+  }
+  
+  // Result should be 
+  return 0;
+}
