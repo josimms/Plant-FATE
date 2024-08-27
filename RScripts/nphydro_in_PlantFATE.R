8dd26319d7b4bacaed8c3753737aef9d631b64f5
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-08-27 14:15:09 +0200

    Graphs added for the Ib parameter

--- RScripts/nphydro_in_PlantFATE.R
+++ RScripts/nphydro_in_PlantFATE.R
@@ -84,13 +84,13 @@ create_lho_params <- function(params_file, i_metFile, a_metFile, co2File = "", i
   lho$set_co2File(co2File)
   if (!is.null(init_co2)) lho$init_co2(init_co2)
   print(c(lho$env$clim_inst$co2, lho$env$clim_acclim$co2))
-  print(lho$) # $infra_translation <- param
+  lho$par0$infra_translation <- param # $infra_translation <- param
   lho$init()
   return(lho)
 }
 
 sa_ib <- list()
-potential_parameters = 1:2
+potential_parameters = seq(0.1, 2, length.out = 20)
 for (i in potential_parameters) {
   lho_temp <- create_lho_params("tests/params/p_test_v2.ini", 
                          "tests/data/MetData_AmzFACE_Monthly_2000_2015_PlantFATE_new.csv", 
@@ -101,6 +101,27 @@ for (i in potential_parameters) {
   sa_ib[[which(potential_parameters == i)]] <- run_for_dataset(lho_temp, 2000, 2015, dt)
   sa_ib[[which(potential_parameters == i)]]$Ib_value <- i
 }
+
+sa_ib_all <- do.call(rbind, sa_ib)
+
+par(mfrow = c(2, 2))
+plot(sa_ib_all$date, sa_ib_all$height, ylab = "Height", xlab = "Date", pch = "-", col = sa_ib_all$Ib_value)
+legend("topleft", legend = unique(sa_ib_all$Ib_value), col = unique(sa_ib_all$Ib_value), lty = 1, cex = 0.75, bty = "n", title = "Ib Value")
+plot(sa_ib_all$date, sa_ib_all$root_mass + sa_ib_all$coarse_root_mass, ylab = "Root Mass", xlab = "Date", pch = "-", col = sa_ib_all$Ib_value)
+plot(NULL,
+     xlim = c(sa_ib_all$date[1], sa_ib_all$date[nrow(sa_ib_all)]),
+     ylim = 0.5 + c(0, 0.47),
+     axes = FALSE,
+     xlab = "", ylab = "")
+mtext("Ib", side = 4, line = 3, cex = 0.8)
+legend("topleft", c("Root Mass", "Translation = 2", "Translation = 0.01"), 
+       col = c("black", cols[1], cols[length(translation)]), 
+       lty = 1, bty = "n", cex = 0.75)
+lines(df_Ib$date, df_Ib$root_mass, lty = 2)
+plot(sa_ib_all$date, sa_ib_all$lai, ylab = "LAI", xlab = "Date", type = "l")
+lines(df_Ib$date, df_Ib$lai, lty = 2)
+plot(sa_ib_all$date, sa_ib_all$assim_gross, ylab = "Assim gross", xlab = "Date", type = "l", ylim = c(0, max(sa_ib_all$assim_gross)))
+lines(df_Ib$date, df_Ib$assim_gross, lty = 2)
 
 ###
 # Plant-FATE aggregation
