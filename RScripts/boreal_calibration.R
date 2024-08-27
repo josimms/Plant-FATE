82b99025c3dae5b30fb7c51f67644bac50159bf2
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-08-27 17:58:08 +0200

    Added some final plots to help me think about final outputs

--- RScripts/boreal_calibration.R
+++ RScripts/boreal_calibration.R
@@ -1,3 +1,14 @@
+###
+# Library
+###
+
+library(ggplot2)
+library(patchwork)
+
+###
+# PlantFATE
+###
+
 # Helper function to create and run simulation
 run_simulation <- function(param_file, start_year, end_year, output_prefix) {
   sim <- new(pfate::Patch, param_file)
@@ -144,7 +155,9 @@ boreal_calibration <- function() {
 # Plotting function - not for Life History
 ###
 
-plot_plant_trajectory = function(dat, df_high_co2, df_medium_co2, start_year, end_year){
+plot_plant_trajectory = function(df, df_high_co2, df_medium_co2, start_year, end_year){
+  # TODO: Fix the calibration data
+  
   ####
   # Reading Data
   ####
@@ -156,17 +169,16 @@ plot_plant_trajectory = function(dat, df_high_co2, df_medium_co2, start_year, en
   ####
   # Calculating extra traits
   ####
-  dat$leaf_area = dat$crown_area * dat$lai
-  dat$heartwood_fraction = 1-dat$sapwood_fraction
+  df$leaf_area = df$crown_area * df$lai
+  df$heartwood_fraction = 1-df$sapwood_fraction
   
-  dat$date <- seq(as.Date(paste0(start_year, "-01-01")), as.Date(paste0(end_year, "-01-01")), by = "month")
-  
+  df$date <- as.Date(df$date)
   
   ####
   # Plotting
   ####
   par(mfrow=c(4,2), mar=c(4,4,1,1), oma=c(1,1,1,1))
-  plot(dat$height~dat$diameter, type="l", ylab="Height", xlab="Diameter")
+  plot(df$height~df$diameter, type="l", ylab="Height", xlab="Diameter")
   points(0.01*boreal$smearII_data$amount[boreal$smearII_data$variable == "pine diameter BA weighted mean"],
          boreal$smearII_data$amount[boreal$smearII_data$variable == "pine height BA weighted mean"])
   points(0.01*boreal$smearII_data$amount[boreal$smearII_data$variable == "pine DBH arithmetic mean"],
@@ -176,12 +188,12 @@ plot_plant_trajectory = function(dat, df_high_co2, df_medium_co2, start_year, en
   lines(df_high_co2$height~df_high_co2$diameter, col = "red")
   lines(df_medium_co2$height~df_medium_co2$diameter, col = "orange")
   
-  plot(dat$crown_area~I(dat$height*dat$diameter), type="l", ylab="Crown area", xlab="DH")
-  plot(I(dat$total_rep/dat$total_prod)~dat$height, type="l", ylab="Frac alloc to\nreproduction", xlab="Height")
-  plot(I(dat$total_rep/dat$total_prod)~dat$diameter, type="l", ylab="Frac alloc to\nreproduction", xlab="Diameter")
+  plot(df$crown_area~I(df$height*df$diameter), type="l", ylab="Crown area", xlab="DH")
+  plot(I(df$total_rep/df$total_prod)~df$height, type="l", ylab="Frac alloc to\nreproduction", xlab="Height")
+  plot(I(df$total_rep/df$total_prod)~df$diameter, type="l", ylab="Frac alloc to\nreproduction", xlab="Diameter")
   
   par(mfrow=c(4,4), mar=c(4,8,1,1), oma=c(1,1,1,1), mgp=c(4,1,0), cex.lab=1.2)
-  plot(dat$height~dat$date, ylab="Height", xlab="Year", type="l", lwd=2)
+  plot(df$height~df$date, ylab="Height", xlab="Year", type="l", lwd=2)
   points(boreal$smearII_data$date[boreal$smearII_data$variable == "pine height BA weighted mean"],
          boreal$smearII_data$amount[boreal$smearII_data$variable == "pine height BA weighted mean"])
   points(boreal$smearII_data$date[boreal$smearII_data$variable == "pine height arithmetic mean"],
@@ -189,7 +201,7 @@ plot_plant_trajectory = function(dat, df_high_co2, df_medium_co2, start_year, en
   points(boreal$smearII_data$date[boreal$smearII_data$variable == "BA weighted mean height pine"],
          boreal$smearII_data$amount[boreal$smearII_data$variable == "BA weighted mean height pine"], pch = 3)
   
-  plot(dat$diameter~dat$date, ylab="Diameter", xlab="Year", col="brown", type="l", lwd=2)
+  plot(df$diameter~df$date, ylab="Diameter", xlab="Year", col="brown", type="l", lwd=2)
   points(boreal$smearII_data$date[boreal$smearII_data$variable == "pine diameter BA weighted mean"],
          0.01 * boreal$smearII_data$amount[boreal$smearII_data$variable == "pine diameter BA weighted mean"])
   points(boreal$smearII_data$date[boreal$smearII_data$variable == "pine DBH arithmetic mean"],
@@ -197,12 +209,12 @@ plot_plant_trajectory = function(dat, df_high_co2, df_medium_co2, start_year, en
   points(boreal$smearII_data$date[boreal$smearII_data$variable == "BA weighted mean DBH pine"],
          0.01 * boreal$smearII_data$amount[boreal$smearII_data$variable == "BA weighted mean DBH pine"], pch = 3)
   
-  matplot(y=cbind(dat$total_mass,
-                  dat$total_rep,
-                  dat$litter_mass,
-                  dat$total_mass+dat$total_rep+dat$litter_mass,
-                  dat$total_prod), 
-          x=dat$date, col=c("green4", "purple", "yellow4", "black", "red"), log="", lty=c(1,1,1,1,2), lwd=c(1,1,1,2,1), type="l",
+  matplot(y=cbind(df$total_mass,
+                  df$total_rep,
+                  df$litter_mass,
+                  df$total_mass+df$total_rep+df$litter_mass,
+                  df$total_prod), 
+          x=df$date, col=c("green4", "purple", "yellow4", "black", "red"), log="", lty=c(1,1,1,1,2), lwd=c(1,1,1,2,1), type="l",
           ylab="Biomass pools", xlab="Year")
   abline(h=0, col="grey")
   points(boreal$smearII_data$date[boreal$smearII_data$variable == "pine diameter BA weighted mean"],
@@ -213,18 +225,18 @@ plot_plant_trajectory = function(dat, df_high_co2, df_medium_co2, start_year, en
          0.01 * boreal$smearII_data$amount[boreal$smearII_data$variable == "BA weighted mean DBH pine"], pch = 3)
   
   
-  matplot(y=cbind(dat$total_mass,
-                  dat$leaf_mass,
-                  dat$root_mass,
-                  dat$coarse_root_mass,
-                  dat$stem_mass),
-          x=dat$date, col=c("black", "green3", "purple", "purple3", "brown"), log="", lty=c(1,1,1,1,1), lwd=c(1,1,1,1,1), type="l",
+  matplot(y=cbind(df$total_mass,
+                  df$leaf_mass,
+                  df$root_mass,
+                  df$coarse_root_mass,
+                  df$stem_mass),
+          x=df$date, col=c("black", "green3", "purple", "purple3", "brown"), log="", lty=c(1,1,1,1,1), lwd=c(1,1,1,1,1), type="l",
           ylab="Biomass pools", xlab="Year")
   abline(h=0, col="grey")
   
   
-  matplot(y=cbind(dat$fitness), 
-          x=dat$date, col=c("purple2", "magenta", "pink"), log="", lty=c(1,1,1), lwd=c(1,1,2), type="l",
+  matplot(y=cbind(df$fitness), 
+          x=df$date, col=c("purple2", "magenta", "pink"), log="", lty=c(1,1,1), lwd=c(1,1,2), type="l",
           ylab="Fitness", xlab="Year")
   abline(h=0, col="grey")
   
@@ -239,52 +251,52 @@ plot_plant_trajectory = function(dat, df_high_co2, df_medium_co2, start_year, en
   # plot(dat$assim_gross[901:1000]~dat$ppfd[901:1000], type="l")
   
   
-  matplot(y=cbind(dat$assim_gross/dat$crown_area,
-                  dat$assim_net/dat$crown_area,
-                  dat$assim_net/dat$assim_gross * max(dat$assim_gross/dat$crown_area)), 
-          x=dat$date, col=c("green3", "green4", scales::alpha("yellow3", 0.5)), log="", lty=1, type="l", lwd=c(1,1,3),
+  matplot(y=cbind(df$assim_gross/df$crown_area,
+                  df$assim_net/df$crown_area,
+                  df$assim_net/df$assim_gross * max(df$assim_gross/df$crown_area)), 
+          x=df$date, col=c("green3", "green4", scales::alpha("yellow3", 0.5)), log="", lty=1, type="l", lwd=c(1,1,3),
           ylab=expression(atop("GPP, NPP", "(kg m"^"-2"*"Yr"^"-1"*")")), xlab="Year")
   abline(h=0, col="grey")
   lines(ERAS_Monthly$date, ERAS_Monthly$GPP) # TODO:  µmol m⁻² s⁻¹
   
-  matplot(y=cbind(dat$rr/dat$crown_area,
-                  dat$rs/dat$crown_area,
-                  dat$rl/dat$crown_area), 
-          x=dat$date, col=c("pink2", "pink3", "pink4"), log="", lty=1, type="l",
+  matplot(y=cbind(df$rr/df$crown_area,
+                  df$rs/df$crown_area,
+                  df$rl/df$crown_area), 
+          x=df$date, col=c("pink2", "pink3", "pink4"), log="", lty=1, type="l",
           ylab="Respiration", xlab="Year")
   points(boreal$trenching_co2_fluxes$V1,
          boreal$trenching_co2_fluxes$V2, pch = 3) # TODO: check that the units are right and that the right fluxes are used
   
-  matplot(y=cbind(dat$tr/dat$crown_area,
-                  dat$tl/dat$crown_area), 
-          x=dat$date, col=c("orange3", "orange4"), log="", lty=1, type="l",
+  matplot(y=cbind(df$tr/df$crown_area,
+                  df$tl/df$crown_area), 
+          x=df$date, col=c("orange3", "orange4"), log="", lty=1, type="l",
           ylab="Turnover", xlab="Year",
           add=F)
   
-  plot(I(dat$transpiration/dat$crown_area/1000*1000)~dat$date, type="l", col="blue", ylab="Transpitation\n(mm/yr)")
+  plot(I(df$transpiration/df$crown_area/1000*1000)~df$date, type="l", col="blue", ylab="Transpitation\n(mm/yr)")
   
-  plot(dat$dpsi~dat$date, type="l", col="cyan", ylab=expression(atop(Delta*psi, "(MPa)")), xlab="Year")
+  plot(df$dpsi~df$date, type="l", col="cyan", ylab=expression(atop(Delta*psi, "(MPa)")), xlab="Year")
   
-  plot(dat$vcmax~dat$date, type="l", col="limegreen", ylab=expression(atop(V[cmax], "("*mu*"mol m"^"-2"*"s"^"-1"*")")), xlab="Year")
+  plot(df$vcmax~df$date, type="l", col="limegreen", ylab=expression(atop(V[cmax], "("*mu*"mol m"^"-2"*"s"^"-1"*")")), xlab="Year")
   
-  plot(I(dat$leaf_area/dat$crown_area)~dat$date, ylab="LAI", xlab="Year", type="l")
+  plot(I(df$leaf_area/df$crown_area)~df$date, ylab="LAI", xlab="Year", type="l")
   
-  summary(dat$mortality)
+  summary(df$mortality)
   
-  plot(I(dat$mortality)~dat$date, ylab="Cumulative\nMortality", xlab="Year", type="l")
+  plot(I(df$mortality)~df$date, ylab="Cumulative\nMortality", xlab="Year", type="l")
   
-  plot(I(dat$mortality_inst[dat$diameter<0.5])~dat$diameter[dat$diameter<0.5], ylab="Instantaneous\nmortality rate", xlab="Diameter", type="l")
+  plot(I(df$mortality_inst[df$diameter<0.5])~df$diameter[df$diameter<0.5], ylab="Instantaneous\nmortality rate", xlab="Diameter", type="l")
   
-  matplot(y=cbind(dat$sapwood_fraction, 
-                  dat$heartwood_fraction),
-          x=dat$date, 
+  matplot(y=cbind(df$sapwood_fraction, 
+                  df$heartwood_fraction),
+          x=df$date, 
           ylab="Sap/Heart wood\nfraction", xlab="Year", type="l", col=c("yellowgreen", "brown4"), lty=1, lwd=1)
   
   # plot(I(dat$ppfd)~dat$i, ylab="PPFD", xlab="Year", type="l", col="yellowgreen")
   
-  matplot(y=(cbind(dat$leaf_lifespan, 
-                   dat$fineroot_lifespan)),
-          x=dat$height, 
+  matplot(y=(cbind(df$leaf_lifespan, 
+                   df$fineroot_lifespan)),
+          x=df$height, 
           ylab="Leaf/fine root\nlifespan", xlab="Height", type="l", col=c("yellowgreen", "brown4"), lty=1, lwd=1)
   abline(h=1, col="grey")
 }
@@ -303,15 +315,6 @@ create_lho <- function(params_file, i_metFile, a_metFile, co2File = "", init_co2
   lho$init()
   return(lho)
 }
-
-###
-# PlantFATE output is a file so look at
-#
-# Look here to change the directory for each param 
-#class Patch{
-#  public:
-#    PlantFateConfig config;
-###
 
 run_for_dataset <- function(lho, start_year, end_year, dt) {
   df <- data.frame(matrix(ncol = length(lho$get_header()), nrow = 0))
@@ -356,5 +359,86 @@ life_history <- function() {
   df_medium_co2 <- run_for_dataset(lho_medium_co2, 2015, 2100, dt)
   
   # Plot results
-  plot_plant_trajectory(df, df_high_co2, df_medium_co2, 1960, 2022)
+  # TODO: boreal calibration - with the biomass data
+  plot_plant_trajectory(df, df_high_co2, df_medium_co2, 1960, 1995)
+  
+  # Future scenarios
+  
+}
+
+
+future_scenarios <- function(df, df_medium_co2, df_high_co2) {
+  crossover_days = seq(as.Date("2015-01-01"), as.Date("2022-01-01"), by = "month")
+ 
+  ###
+  # Checking the fluxes!
+  ###
+  
+  par(mfrow = c(2, 3))
+  ### Assimilation
+  plot(df$date, df$assim_gross, type = "l", xlab = "Time", ylab = "Assimulation Gross")
+  
+  plot(df_high_co2$date[df_high_co2$date %in% crossover_days], 
+       df_high_co2$assim_gross[df_high_co2$date %in% crossover_days], 
+       col = "red", type = "l", xlab = "Time", ylab = "Assimulation Gross")
+  lines(df_medium_co2$date[df_medium_co2$date %in% crossover_days],
+        df_medium_co2$assim_gross[df_medium_co2$date %in% crossover_days], 
+        col = "yellow", type = "l")
+  lines(df$date[df$date == crossover_days], 
+        df$assim_gross[df$date == crossover_days], 
+        type = "l")
+  
+  plot(df_high_co2$date, df_high_co2$assim_gross, col = "red", type = "l", xlab = "Time", ylab = "Assimulation Gross")
+  lines(df_medium_co2$date, df_medium_co2$assim_gross, col = "yellow", type = "l")
+  
+  ### Total Respiration
+  plot(df$date, df$rl + df$rs + df$rr, type = "l", xlab = "Time", ylab = "Total Respiration")
+  
+  plot(df_high_co2$date[df_high_co2$date %in% crossover_days], 
+       df_high_co2$rl[df_high_co2$date %in% crossover_days] +
+         df_high_co2$rs[df_high_co2$date %in% crossover_days] +
+         df_high_co2$rr[df_high_co2$date %in% crossover_days], 
+       col = "red", type = "l", xlab = "Time", ylab = "Assimulation Gross")
+  lines(df_medium_co2$date[df_medium_co2$date %in% crossover_days],
+        df_medium_co2$rl[df_medium_co2$date %in% crossover_days] + 
+          df_medium_co2$rs[df_medium_co2$date %in% crossover_days] +
+          df_medium_co2$rr[df_medium_co2$date %in% crossover_days], 
+        col = "yellow", type = "l")
+  lines(df$date[df$date == crossover_days], 
+        df$rl[df$date == crossover_days] + 
+          df$rs[df$date == crossover_days] + 
+          df$rr[df$date == crossover_days], 
+        type = "l")
+  
+  plot(df_high_co2$date, df_high_co2$rl + df_high_co2$rs + df_high_co2$rr, col = "red", 
+       type = "l", xlab = "Time", ylab = "Total Respiration")
+  lines(df_medium_co2$date, df_medium_co2$rl + df_medium_co2$rs + df_medium_co2$rr, col = "yellow", type = "l")
+  
+  ###
+  # Difference in biomass under all scenarios
+  ###
+  
+  # Function to create individual plots
+  create_plot <- function(high_df, medium_df, y_var, y_label) {
+    ggplot() +
+      geom_line(data = high_df, aes(x = date, y = .data[[y_var]]), color = "red") +
+      geom_line(data = medium_df, aes(x = date, y = .data[[y_var]]), color = "yellow") +
+      labs(x = "Date", y = y_label) +
+      theme_minimal()
+  }
+  
+  # Create individual plots
+  p1 <- create_plot(df_high_co2, df_medium_co2, "height", "Height")
+  p2 <- create_plot(df_high_co2, df_medium_co2, "diameter", "Diameter")
+  p3 <- create_plot(df_high_co2, df_medium_co2, "total_mass", "Total Mass")
+  p4 <- create_plot(df_high_co2, df_medium_co2, "crown_area", "Crown Area")
+  p_nitrogen <- create_plot(df_high_co2, df_medium_co2, "nitrogen", "Leaf Nitrogen (Jamx)") +
+    theme(axis.title.x = element_text(margin = margin(t = 10)))  # Add some margin to the x-axis label
+  
+  # Combine plots
+  combined_plot <- (p1 + p2) / (p3 + p4) / p_nitrogen +
+    plot_layout(heights = c(1, 1, 1))  # Ensure equal height for all rows
+  
+  # Display the combined plot
+  print(combined_plot)
 }
