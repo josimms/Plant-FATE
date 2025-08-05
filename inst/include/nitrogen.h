feb8d2691d72136ee853e1f3346c05781dcc7a51
Joanna Simms <joanna.x.simms@helsinki.fi>
2025-08-05 17:54:27 +0300

    Taken the investment_tree parameter out as it is not in this structure anymore and added the brief description structure to the uptake class

--- inst/include/nitrogen.h
+++ inst/include/nitrogen.h
@@ -5,7 +5,7 @@ namespace plant {
 
 class Uptake {
 public:
-  // Parameters (types guessed — update as needed)
+  // Parameters
   double k_8;
   double k_9;
   double k_10;
@@ -14,7 +14,6 @@ public:
   double k_13;
   double mycorrhized;
   double investment_myco;
-  double investment_tree;
   double u_c_B;
   double e_u_root;
   double e_u_myco;
@@ -22,11 +21,15 @@ public:
   
 public:
   // Uptake equations
+  
+  /// @{brief Uptake Functions
   double uptake_myco(double N);
   double uptake_roots(double N, PlantArchitecture& G, PlantTraits& T);
   double uptake_age(const PlantArchitecture& G, PlantTraits& T);
   double nitrogen_gate(const PlantArchitecture& G, PlantTraits& T);
   double nitrogen_plant(double N, PlantArchitecture& G, PlantTraits& T);
+  /// @}
+  
   void init(io::Initializer& I);
 };
 
