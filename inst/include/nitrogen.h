df482445587be1a3b46d10a460d70be3868385f8
Joanna Simms <joanna.x.simms@helsinki.fi>
2025-08-05 13:54:53 +0300

    Corrected the functions for the nitrogen to include the PlantTraits T, the correct hedaers and the init uptake function added for the plant

--- inst/include/nitrogen.h
+++ inst/include/nitrogen.h
@@ -1,34 +1,37 @@
+#include "plant_architecture.h"
+#include "utils/initializer_v2.h"
+
 namespace plant {
 
-  class nitrogen {
+class Uptake {
+public:
+  // Parameters (types guessed — update as needed)
+  double k_8;
+  double k_9;
+  double k_10;
+  double k_11;
+  double k_12;
+  double k_13;
+  double mycorrhized;
+  double investment_myco;
+  double u_c_B;
+  double e_u_root;
+  double e_u_myco;
+  double q;
   
-    public:
-      
-    struct {
-      // Parameters
-      k_8;
-      k_9;
-      k_10;
-      k_11;
-      k_12;
-      k_13;
-      mycorrhized;
-      investment_myco;
-      // Nitrogen state
-      tree_nitrogen;
-    } uptake;
-      
-    public:
-    // Uptake equations
-    double uptake_myco(double N);
-    double uptake_roots(double N, const PlantArchitecture& G);
-    double uptake_age(const PlantArchitecture& G);
-    double nitrogen_gate(const PlantArchitecture& G);
-    double nitrogen_plant(double N, const PlantArchitecture& G, uptake U);
+  // Nitrogen state
+  double tree_nitrogen;
   
-  };
+public:
+  // Uptake equations
+  double uptake_myco(double N);
+  double uptake_roots(double N, PlantArchitecture& G, PlantTraits& T);
+  double uptake_age(const PlantArchitecture& G, PlantTraits& T);
+  double nitrogen_gate(const PlantArchitecture& G, PlantTraits& T);
+  double nitrogen_plant(double N, PlantArchitecture& G, PlantTraits& T);
+  void init(io::Initializer& I);
+};
 
-
-}
+} // end of namespace
 
   
--- src/nitrogen.cpp
+++ src/nitrogen.cpp
@@ -3,74 +3,76 @@ using namespace std;
 
 namespace plant {
 
-  double nitrogen::uptake_myco(double N) {
+  double Uptake::uptake_myco(double N) {
     // TODO: can I pull this from somewhere?
     double myco_biomass;
     
-    // Parameters
-    double k_12;
-    double u_c_B;
-    double e_u_myco;
-    
-    double biomass_limitation = myco_biomass / (myco_biomass + 2.0*k_12);
+    double biomass_limitation = myco_biomass / (myco_biomass + 2.0 * k_12);
     double nitrogen_uptake = (u_c_B * N * e_u_myco) / (u_c_B + N * e_u_myco);
     
     double nitrogen_capacity = myco_biomass/2.0 * biomass_limitation * nitrogen_uptake; 
     
-    return nitrogen_capacity * N / (nitrogen_capapcity + N);
+    return nitrogen_capacity * N / (nitrogen_capacity + N);
   }
   
-  double nitrogen::uptake_roots(double N, const PlantArchitecture& G) {
+  double Uptake::uptake_roots(double N, PlantArchitecture& G, PlantTraits& T) {
     double root_length = G.root_length;
     double root_no = G.root_no;
     
-    // TODO: can I pull this from somewhere?
-    double root_biomass;
+    double root_mass_calc = G.root_mass(T);
     
-    // Parameters
-    double k_13;
-    double u_c_B;
-    double e_u_root;
-    
-    double surface_area = root_no * M_PI * root_length * root_diameter(root_length);
-    double biomass = root_no * M_PI * pow(root_length/2.0, 2.0) * 
+    double surface_area = root_no * M_PI * root_length * G.root_diameter(T);
       
-    double biomass_conversion = 4 * biomass / (root_denisty(root_length) * root_diameter(root_length));
-    double biomass_limitation = surface_area / (surface_area + 2.0*k_13);
+    double biomass_conversion = 4 * root_mass_calc / (G.root_density(T) * G.root_diameter(T));
+    double biomass_limitation = surface_area / (surface_area + 2.0 * k_13);
     double nitrogen_uptake = (u_c_B * N * e_u_root) / (u_c_B + N * e_u_root);
     
-    double nitrogen_capacity = biomass_conversion * biomass_limitation * nitrogen_uptake
+    double nitrogen_capacity = biomass_conversion * biomass_limitation * nitrogen_uptake;
     
     return nitrogen_capacity * N / (nitrogen_capacity + N);
   }
   
-  double nitrogen::uptake_age(const PlantArchitecture& G) {
-    double root_length = G.root_length;
-    // TODO: this should join some sort of structure
-    double k_8;
-    
-    return 1/(1 + exp(root_lifespan(root_length)) - k_8);
+  double Uptake::uptake_age(const PlantArchitecture& G, PlantTraits& T) {
+    return 1/(1 + exp(G.root_lifespan(T)) - k_8);
   }
   
-  double nitrogen::nitrogen_gate(const PlantArchitecture& G) {
+  double Uptake::nitrogen_gate(const PlantArchitecture& G, PlantTraits& T) {
     double root_length = G.root_length;
     double root_no = G.root_no;
     
-    double surface_area = root_no * M_PI * root_length * root_diameter(root_length);
+    double surface_area = root_no * M_PI * root_length * G.root_diameter(T);
     
     return surface_area/(surface_area + k_9);
   }
   
-  double nitrogen::nitrogen_plant(double N, const PlantArchitecture& G, uptake parameters) {
+  double Uptake::nitrogen_plant(double N, PlantArchitecture& G, PlantTraits& T) {
     double root_length = G.root_length;
     double root_no = G.root_no;
     
-    double age_effect = uptake_age(root_length);
+    double age_effect = uptake_age(G, T);
     
-    uptake_roots_term = (1 - parameters.mycorrhized) * uptake_roots(N, G); 
-    uptake_mycorrhiza_term = parameters.mycorrhized * nitrogen_gate(G) * parameters.investment_myco * uptake_mycorrhiza(N);
+    double uptake_roots_term = (1 - mycorrhized) * uptake_roots(N, G, T); 
+    double uptake_mycorrhiza_term = mycorrhized * nitrogen_gate(G, T) * investment_myco * uptake_myco(N);
     
     return age_effect * (uptake_roots_term + uptake_mycorrhiza_term);
   }
 
-}
+  void Uptake::init(io::Initializer& I){
+    
+    // Nitrogen parameters
+    k_8                 = I.get<double>("k_8");
+    k_9                 = I.get<double>("k_9");
+    k_10                = I.get<double>("k_10");
+    k_11                = I.get<double>("k_11");
+    k_12                = I.get<double>("k_12");
+    k_13                = I.get<double>("k_13");
+    mycorrhized         = I.get<double>("mycorrhized");
+    investment_myco     = I.get<double>("investment_myco");
+    u_c_B               = I.get<double>("u_c_B");
+    e_u_root            = I.get<double>("e_u_root");
+    e_u_myco            = I.get<double>("e_u_myco");
+    q                   = I.get<double>("q");
+    
+  }
+
+} // End namespace
