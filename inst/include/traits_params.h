fdd8c3864ef16e7e2b14f3ad92d02b77f4449d58
Joanna Simms <joanna.x.simms@helsinki.fi>
2025-08-11 20:40:51 +0300

    Nitrogen uptake inilisation

--- inst/include/traits_params.h
+++ inst/include/traits_params.h
@@ -96,6 +96,7 @@ class PlantParameters{
 	double root_no0;		        ///< Initial number of root tips
 	double root_length0;		    ///< Initial root length
 	double nitrogen_start0;     ///< Initial nitrogen
+	double nitrogen_uptake0;    ///< Initial uptake
 	
 	// Allocation and geometric paramaters  
 	double fg;		              ///< upper canopy gap fraction
