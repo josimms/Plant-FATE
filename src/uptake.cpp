11149d9380b8159b8a5b08ce165da39e119be721
Joanna Simms <joanna.x.simms@helsinki.fi>
2025-08-12 15:39:17 +0300

    Unit conversions corrected so that mm and m are consistant between the density and the diameter.
    now the biomass should be in kg and the ectomycorrhizal biomass made dynamic

--- src/uptake.cpp
+++ src/uptake.cpp
@@ -3,15 +3,13 @@ using namespace std;
 
 namespace plant {
 
-  double Uptake::uptake_myco(double N) {
-    // TODO: can I pull this from somewhere?
-    double myco_biomass;
-    
-    double biomass_limitation = myco_biomass / (myco_biomass + 2.0 * k_12);
+  double Uptake::uptake_myco(double N, PlantArchitecture& G) {
+    double biomass_limitation = G.ectomycorrhiza_mass / (G.ectomycorrhiza_mass + 2.0 * k_12);
     double nitrogen_uptake = (u_c_B * N * e_u_myco) / (u_c_B + N * e_u_myco);
     
-    double nitrogen_capacity = myco_biomass/2.0 * biomass_limitation * nitrogen_uptake; 
+    double nitrogen_capacity = G.ectomycorrhiza_mass * biomass_limitation * nitrogen_uptake; 
     
+    // gN per kg C
     return nitrogen_capacity * N / (nitrogen_capacity + N);
   }
   
@@ -19,16 +17,15 @@ namespace plant {
     double root_length = G.root_length;
     double root_no = G.root_no;
     
-    double root_mass_calc = G.root_mass(T);
-    
-    double surface_area = root_no * M_PI * root_length * G.root_diameter(T);
+    double surface_area = root_no * M_PI * (root_length * 1e-3) * (G.root_diameter(T) * 1e-3); // m
       
-    double biomass_conversion = 4 * root_mass_calc / (G.root_density(T) * G.root_diameter(T));
+    double biomass_conversion = 4 * G.root_mass(T) / (G.root_density(T) * G.root_diameter(T) * 1e-3); // kg / (kg-1/m3 * mm * 1e-3) = m2
     double biomass_limitation = surface_area / (surface_area + 2.0 * k_13);
     double nitrogen_uptake = (u_c_B * N * e_u_root) / (u_c_B + N * e_u_root);
     
     double nitrogen_capacity = biomass_conversion * biomass_limitation * nitrogen_uptake;
     
+    // gN per kg C
     return nitrogen_capacity * N / (nitrogen_capacity + N);
   }
   
@@ -52,7 +49,7 @@ namespace plant {
     double age_effect = uptake_age(G, T);
     
     double uptake_roots_term = (1 - mycorrhized) * uptake_roots(N, G, T); 
-    double uptake_mycorrhiza_term = mycorrhized * nitrogen_gate(G, T) * investment_from_myco * uptake_myco(N);
+    double uptake_mycorrhiza_term = mycorrhized * nitrogen_gate(G, T) * investment_from_myco * uptake_myco(N, G);
     
     return age_effect * (uptake_roots_term + uptake_mycorrhiza_term);
   }
