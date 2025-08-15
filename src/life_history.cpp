df1d8ec41f0c8c7452f94f637ee62f3f0d4db41f
Joanna Simms <joanna.x.simms@helsinki.fi>
2025-08-15 17:31:47 +0300

    Scaled uptake

--- src/life_history.cpp
+++ src/life_history.cpp
@@ -254,7 +254,7 @@ void LifeHistoryOptimizer::grow_for_dt(double t, double dt) {
     
     // 3. Nitrogen uptake & balance calculations
     P.geometry.nitrogen_uptake = P.uptake.nitrogen_plant(C.clim_acclim.nitrogen, P.geometry, P.traits);
-    P.geometry.nitrogen_tree  += P.geometry.nitrogen_uptake * P.traits.zeta; 
+    P.geometry.nitrogen_tree  += P.geometry.nitrogen_uptake * P.traits.zeta * P.geometry.crown_area;
     P.geometry.potential_nitrogen_leaf = P.geometry.nitrogen_leaf(P.geometry.nitrogen_tree, P.traits);
     
     double dN_dt_growth = 0.0;
