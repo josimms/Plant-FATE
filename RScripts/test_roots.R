cdb7fb738377095286b9b1b1f58ceefc0485b5f9
Joanna Simms <joanna.x.simms@helsinki.fi>
2025-10-08 13:36:13 +0300

    Added graphs

--- RScripts/test_roots.R
+++ RScripts/test_roots.R
@@ -1,4 +1,10 @@
 blank <- function() {
+  library(dplyr)
+  library(lubridate)  # convenient for date operations
+  library(ggplot2)
+  library(zoo)
+  library(data.table)
+  
   ###
   # Simulation!
   ###
@@ -54,18 +60,45 @@ blank <- function() {
   data_directory <- "~/Documents/CASSIA_Calibration/Processed_Data/"
   loaded_data <- CASSIA::load_data(data_directory)
 
-  Eddy_covariance <- read.csv("~/Documents/CASSIA/data/daily_dataframe.csv")
   input_climate <- read.csv("~/Documents/Austria/Plant-FATE/tests/data/ERAS_Monthly.csv")
-  # Sum of daily data in TODO: units?
-  Eddy_covariance_monthly <- aggregate(GPP_mean ~ Year + Month, 
-                                       data = Eddy_covariance, 
-                                       FUN = sum)
-  # Create a proper Date column (first day of each month)
-  Eddy_covariance_monthly$Date <- as.Date(paste(Eddy_covariance_monthly$Year, 
-                                                Eddy_covariance_monthly$Month, 
-                                                "01", sep = "-"))
-  # Order by Date
-  Eddy_covariance_monthly <- Eddy_covariance_monthly[order(Eddy_covariance_monthly$Date), ]
+  
+  raw.directory = "/home/josimms/Documents/CASSIA_Calibration/Raw_Data/hyytiala_weather/"
+  environmental.variable.list <- list()
+  count = 1
+  for (variable in c("GPP", "NEE")) {
+    environmental.variable.list[[count]] <- data.table::rbindlist(lapply(paste0(raw.directory, list.files(raw.directory, variable)), data.table::fread))
+    environmental.variable.list[[count]]$Date <- paste(environmental.variable.list[[count]]$Year,
+                                                       environmental.variable.list[[count]]$Month, 
+                                                       environmental.variable.list[[count]]$Day,
+                                                       sep = "-")
+    environmental.variable.list[[count]]$Monthly <- paste(environmental.variable.list[[count]]$Year,
+                                                          environmental.variable.list[[count]]$Month,
+                                                          sep = "-")
+    count = count + 1
+  }
+  names(environmental.variable.list) <- c("GPP", "NEE")
+  
+  # Original Eddy Covarience files in umol CO2 m-2 s-1
+  DT <- copy(environmental.variable.list[["GPP"]])   # copy to avoid modifying original
+  DT2 <- copy(environmental.variable.list[["NEE"]])   # copy to avoid modifying original
+  GPP_out <- DT[, .(GPP_mean = mean(HYY_EDDY233.GPP, na.rm = TRUE)), by = Monthly]
+  NEE_out <- DT2[, .(NEE_mean = mean(HYY_EDDY233.NEE, na.rm = TRUE)), by = Monthly]
+  
+  GPP_out <- GPP_out[ , Monthly := zoo::as.yearmon(Monthly, "%Y-%m") ]
+  NEE_out <- NEE_out[ , Monthly := zoo::as.yearmon(Monthly, "%Y-%m") ]
+  
+  # If Month is numeric (1..12):
+  # TODO: is there transpiration data?
+  setorder(GPP_out, Monthly)
+  setorder(NEE_out, Monthly)
+  
+  GPP_out$GPP_mean = GPP_out$GPP_mean * 12 * 1e-6 * 1e-3 * 30 * 24 * 60
+  NEE_out$NEE_mean = NEE_out$NEE_mean * 12 * 1e-6 * 1e-3 * 30 * 24 * 60
+  
+  GPP_out$NEE <- NEE_out$NEE_mean
+  Eddy_covariance <- GPP_out
+  Eddy_covariance[, MonthlyDate := as.Date(paste0("01 ", Monthly), format = "%d %b %Y")]
+  Eddy_covariance[, Month := as.integer(format(MonthlyDate, "%m"))]
   
   ###
   # Plots to get parameters
@@ -90,9 +123,87 @@ blank <- function() {
   ### PHOTOSYNTHESIS
   #######
   par(mfrow = c(2, 2))
-  # TODO: what is happening here?
+  
+  #--------------------------------------
+  # 1. Compute month from dates
+  #--------------------------------------
+  df <- df %>%
+    mutate(Month = month(date))   # month 1–12 from model date
+  
+  input_climate <- input_climate %>%
+    mutate(
+      Date = as.Date(paste(Year, Month, "01", sep = "-")),  # construct full date
+      Month = as.integer(Month)
+    )
+  
+  #--------------------------------------
+  # 2. Summarise monthly PPFD statistics
+  #--------------------------------------
+  monthly_ppfd_model <- df %>%
+    group_by(Month) %>%
+    summarise(
+      mean_ppfd = mean(ppfd, na.rm = TRUE),
+      min_ppfd  = min(ppfd, na.rm = TRUE),
+      max_ppfd  = max(ppfd, na.rm = TRUE)
+    )
+  
+  monthly_ppfd_input <- input_climate %>%
+    group_by(Month) %>%
+    summarise(
+      mean_ppfd = mean(PPFD, na.rm = TRUE),
+      min_ppfd  = min(PPFD, na.rm = TRUE),
+      max_ppfd  = max(PPFD, na.rm = TRUE)
+    )
+  
+  #--------------------------------------
+  # 3. Set y-axis limits across both datasets
+  #--------------------------------------
+  ylim <- range(c(
+    monthly_ppfd_model$min_ppfd, monthly_ppfd_model$max_ppfd,
+    monthly_ppfd_input$min_ppfd, monthly_ppfd_input$max_ppfd
+  ), na.rm = TRUE)
+  
+  #--------------------------------------
+  # 4. Base plot for model (grey shaded range)
+  #--------------------------------------
+  plot(monthly_ppfd_model$Month, monthly_ppfd_model$mean_ppfd, type = "l",
+       ylim = ylim, xlab = "Month", ylab = expression("PPFD ("*mu*"mol m"^-2*" s"^-1*")"),
+       main = "Monthly Mean and Range of PPFD")
+  
+  polygon(
+    c(monthly_ppfd_model$Month, rev(monthly_ppfd_model$Month)),
+    c(monthly_ppfd_model$min_ppfd, rev(monthly_ppfd_model$max_ppfd)),
+    col = "grey80", border = NA
+  )
+  lines(monthly_ppfd_model$Month, monthly_ppfd_model$mean_ppfd, lwd = 2, col = "black")
+  
+  #--------------------------------------
+  # 5. Overlay input_climate data (blue shaded area + line)
+  #--------------------------------------
+  polygon(
+    c(monthly_ppfd_input$Month, rev(monthly_ppfd_input$Month)),
+    c(monthly_ppfd_input$min_ppfd, rev(monthly_ppfd_input$max_ppfd)),
+    col = rgb(0, 0, 1, 0.2), border = NA
+  )
+  lines(monthly_ppfd_input$Month, monthly_ppfd_input$mean_ppfd, col = "blue", lwd = 2)
+  
+  #--------------------------------------
+  # 6. Add axis and legend
+  #--------------------------------------
+  axis(1, at = 1:12, labels = month.abb)
+  legend("topright", legend = c("Model PPFD", "Input Climate PPFD"),
+         col = c("black", "blue"), lty = 1, lwd = 2, bty = "n")
+  
+  #--------------------------------------
+  # 7. Add caption
+  #--------------------------------------
+  mtext("Instantaneous climate data (blue) compared with model output (black)",
+        side = 3, line = 0.5, cex = 0.8)
+  
+  
+  ylim = range(df$ppfd, input_climate$PPFD, na.rm = T)
   plot(as.Date(paste(input_climate$Year, input_climate$Month, 01), format = "%Y%m%d"), input_climate$PPFD, col = "green", main = "PPFD", xlab = "Date", ylab = "?")
-  mtext("Instantenous Climate is outputted", 
+  mtext("Instantenous Climate is outputted. Green: weather data in", 
         side = 3, line = 0.5, cex = 0.8)
   points(df$date, df$ppfd)
   
@@ -135,35 +246,88 @@ blank <- function() {
   lines(1:365, day_length_hours(-60, 1:365), main = "Correction for day length", xlab = "Day of Year", ylab = "Hours of sunlight", col = "red")
   legend("top", legend = c(60, 0, -60), col = c("green", "blue", "red"), lty = 1, bty = "n", title = "Latitude", horiz = TRUE)
   
+  ylim = range(df$assim_net, Eddy_covariance$NEE, na.rm = T)
+  # TODO: i have this data no?
+  plot(df$date, df$assim_net, main = "Net Assimilation", xlab = "Date", ylab = "kg C per month", ylim = ylim)
+  mtext("Eddy, umol CO2 m-2 s-1 * seconds * 12 * 1e-9 = kg C per month", 
+        side = 3, line = 0.5, cex = 0.7, col = "blue")
+  points(Eddy_covariance$MonthlyDate, Eddy_covariance$NEE, col = "blue")
+  
   # PRELES, units: g C m-2 to kg per month per canopy of one tree (TODO: is this m-2 of ground or leaves?)
   # Eddy_covariance_monthly$GPP_mean / 1000,
   # 1000 as a divider to make per tree and then 1000 from g C to kg C
-  ylim = range(df$assim_net, Eddy_covariance_monthly$GPP_mean, na.rm = T)
+  ylim = range(df$assim_gross, Eddy_covariance$GPP_mean, na.rm = T)
   # TODO: is this net or gross assimilation?
   # TODO: although there isn't any nitrogen uptake limitation could this be changing something somehow?
-  plot(df$date, df$assim_net, main = "Net Assimilation", xlab = "Date", ylab = "kg C per month", ylim = ylim)
-  points(as.Date(paste(Eddy_covariance_monthly$Year, Eddy_covariance_monthly$Month, "01", sep = "-")), Eddy_covariance_monthly$GPP_mean, col = "blue")
-  mtext("PRELES, monthly sum of daily sum g C * 1000 / 1000 = kg C per month", 
-        side = 3, line = 0.5, cex = 0.8)
+  plot(df$date, df$assim_gross, main = "Gross Assimilation", xlab = "Date", ylab = "kg C per month", ylim = ylim)
+  points(Eddy_covariance$MonthlyDate, Eddy_covariance$GPP_mean, col = "blue")
+  mtext("Eddy, umol CO2 m-2 s-1 * seconds * 12 * 1e-9 = kg C per month", 
+        side = 3, line = 0.5, cex = 0.7, col = "blue")
   
-  plot(df$date, df$assim_gross, main = "Gross Assimilation", xlab = "Date", ylab = "?")
-  # TODO: is this net or gross assimilation?
+  df <- df %>%
+    mutate(Month = month(date))  # month() returns 1–12
   
-  plot(df$date, df$rl, main = "Leaf respiration", xlab = "Date", ylab = "?")
+  monthly_stats <- df %>%
+    group_by(Month) %>%
+    summarise(
+      mean_assim = mean(assim_gross, na.rm = TRUE),
+      min_assim = min(assim_gross, na.rm = TRUE),
+      max_assim = max(assim_gross, na.rm = TRUE)
+    )
   
-  plot(df$date, df$rr, main = "Root respiration", xlab = "Date", ylab = "?")
+  Eddy_covariance_monthly_stats <- Eddy_covariance %>%
+    group_by(Month) %>%
+    summarise(
+      mean_assim = mean(GPP_mean, na.rm = TRUE),
+      min_assim = min(GPP_mean, na.rm = TRUE),
+      max_assim = max(GPP_mean, na.rm = TRUE)
+    )
   
-  plot(df$date, df$rs, main = "Wood respiration", xlab = "Date", ylab = "?")
+  # Set y-axis limits to include both your model data and Eddy data
+  ylim <- range(c(monthly_stats$min_assim, monthly_stats$max_assim,
+                  Eddy_covariance_monthly_stats$min_assim, 
+                  Eddy_covariance_monthly_stats$max_assim),
+                na.rm = TRUE)
+  # Base plot: model monthly mean and range
+  plot(monthly_stats$Month, monthly_stats$mean_assim, type = "l",
+       ylim = ylim, xlab = "Month", ylab = "Assimilation (kg C per month)",
+       main = "Monthly Gross Assimilation with Range")
+  polygon(c(monthly_stats$Month, rev(monthly_stats$Month)),
+          c(monthly_stats$min_assim, rev(monthly_stats$max_assim)),
+          col = "grey", border = NA)
+  lines(monthly_stats$Month, monthly_stats$mean_assim, lwd = 2)
+  # Overlay Eddy covariance monthly stats (shaded area + mean line)
+  polygon(c(Eddy_covariance_monthly_stats$Month, rev(Eddy_covariance_monthly_stats$Month)),
+          c(Eddy_covariance_monthly_stats$min_assim, rev(Eddy_covariance_monthly_stats$max_assim)),
+          col = rgb(0,0,1,0.2), border = NA)
+  lines(Eddy_covariance_monthly_stats$Month, Eddy_covariance_monthly_stats$mean_assim,
+        col = "blue", lwd = 2)
+  # Add x-axis labels as month abbreviations
+  axis(1, at = 1:12, labels = month.abb)
+  # Add a legend
+  legend("topright", legend = c("Model", "Eddy Covariance"),
+         col = c("black", "blue"), lty = 1, lwd = 2, bty = "n")
   
-  plot(df$date, df$tl, main = "turnover leaf", xlab = "Date", ylab = "?")
+  plot(df$date, df$rl, main = "Leaf respiration", xlab = "Date", ylab = "kg/month")
   
-  plot(df$date, df$tr, main = "turnover roots", xlab = "Date", ylab = "?")
+  ylim = range(df$rr, 30.44 * 4/1000, na.rm = T)
+  plot(df$date, df$rr, main = "Root respiration", xlab = "Date", ylab = "kg/month", ylim = ylim)
+  # ploints()
+  abline(h = 30.44 * 4/1000, col = "blue")
+  # TODO: find the actual flux and add it here, also m2 to root system?
+  mtext("Kira Ryhti dissertation, g C m-2 d-1 x 30.44 / 1000 = kg C m-2 month-1 \n TODO PlantFATE = kg/month (entire root system)", side = 3, line = -1, cex = 0.8)
   
-  plot(df$date, df$dpsi, main = "dpsi", xlab = "Date", ylab = "?")
+  plot(df$date, df$rs, main = "Sapwwod respiration", xlab = "Date", ylab = "kg/month")
   
-  plot(df$date, df$vcmax, main = "Vcmax", xlab = "Date", ylab = "?")
+  plot(df$date, df$tl, main = "turnover leaf", xlab = "Date", ylab = "kg/month")
   
-  plot(df$date, df$transpiration, main = "Transpiration", xlab = "Date", ylab = "?")
+  plot(df$date, df$tr, main = "turnover roots", xlab = "Date", ylab = "kg/month")
+  
+  plot(df$date, df$dpsi, main = "dpsi", xlab = "Date", ylab = "average Pa / month (paper)")
+  
+  plot(df$date, df$vcmax, main = "Vcmax", xlab = "Date", ylab = "Average umol m-2 s-1 per month")
+  
+  plot(df$date, df$transpiration, main = "Transpiration", xlab = "Date", ylab = "kg-h2o month-1")
   
   #######
   ### Biomass
