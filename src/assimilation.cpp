1ec1a70acacb06e5b4575f4f7c5b4ac02abdd6cb
Joanna Simms <joanna.x.simms@helsinki.fi>
2026-05-14 12:41:10 +0300

    Added condition for small vcmax that the lifetime wouldn't expand, but also added kappa_l = 3 for the time being

--- src/assimilation.cpp
+++ src/assimilation.cpp
@@ -6,11 +6,14 @@ namespace plant{
 
 // adjusted for time unit
 void Assimilator::les_update_lifespans(double lai, PlantParameters& par, PlantTraits& traits){
+  // If Vcmax lower then a certain level don't update the les
+  if (plant_assim.vcmax25_avg < 10) return;
+  
 	double hT = plant_assim.vcmax_avg / plant_assim.vcmax25_avg;
 	double f = 1;
 	double fac = sqrt(((par.les_k1 * par.les_k2) * (par.les_k1 * par.les_k2) * f * hT * plant_assim.mc_avg) / (2 * par.les_u * par.les_cc));
 	
-	kappa_l = 365 * par.years_per_tunit_avg * plant_assim.vcmax25_avg / (traits.lma * 1e3 * lai) * fac; // convert yr-1 --> t_unit-1
+	kappa_l = 3; // 365 * par.years_per_tunit_avg * plant_assim.vcmax25_avg / (traits.lma * 1e3 * lai) * fac; // convert yr-1 --> t_unit-1
 	// kappa_r no longer used
 	//kappa_r = 365 * plant_assim.vcmax25_avg / (0.1333 * 1e3) * fac * par.years_per_tunit_avg;           // convert yr-1 --> t_unit-1
 	//kappa_r = kappa_l * (par.les_cc/lai - 1) / (traits.zeta / traits.lma);
